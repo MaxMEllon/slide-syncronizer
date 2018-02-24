7012d1058f56e49d08ce63efaffb90fde1132567
MaxMEllon <maxmellon1994@gmail.com>
2018-02-24 21:08:06 +0900

    Add h1

---
+++ src/components/ui/H1.js
@@ -0,0 +1,50 @@
+import _ from 'lodash'
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const _H1 = styled.h1`
+  display: inline-block;
+  font-size: 4vw;
+  margin-bottom: 3vh;
+  ${props =>
+    props.color &&
+    css`
+      color: ${props.color};
+    `} ${props =>
+      props.underline &&
+      css`
+        border-bottom: ${props.underline};
+      `};
+`
+
+export default class H1 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      underline: 'none',
+      animate: false,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    const { onClick } = this.props
+    if (onClick |> _.isFunction) onClick()
+    if (this.state.animate) return
+    this.setState({ underline: '0.4vh solid red', animate: true })
+    _.delay(() => this.setState({ underline: 'none', animate: false }), 1500)
+  }
+
+  render() {
+    const { children, onClick, ...props } = this.props
+    const { underline } = this.state
+    const newProps = Object.assign({}, props, { underline })
+    return (
+      <div>
+        <_H1 {...props} underline={underline} onClick={this.handleClick}>
+          {children}
+        </_H1>
+      </div>
+    )
+  }
+}
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,20 +1,27 @@
 import _ from 'lodash'
 import { fork, put, call, select, takeLatest } from 'redux-saga/effects'
-import * as actions from '~/actions'
 import { push } from 'react-router-redux'
+import * as actions from '~/actions'
 import { isAdmin } from '~/utils'
 import slideData from '~/constants/slide.json'
+
+const rotateIndexToLeft = currentIndex => {
+  const index = currentIndex - 1
+  if (index === -1) return slideData.pages.length - 1
+  return index
+}
 
 function* pageManageTask() {
   yield takeLatest(actions.pageNext, function*() {
     const { currentPage } = yield select()
-    const index = currentPage.index + 1
+    console.log(slideData.pages.length)
+    const index = (currentPage.index + 1) % slideData.pages.length
     const page = _.get(slideData.pages, index, _.first(slideData.pages))
     yield { page, index } |> actions.changePage |> put
   })
   yield takeLatest(actions.pagePrev, function*() {
     const { currentPage } = yield select()
-    const index = currentPage.index - 1
+    const index = rotateIndexToLeft(currentPage.index)
     const page = _.get(slideData.pages, index, _.last(slideData.pages))
     yield { page, index } |> actions.changePage |> put
   })
