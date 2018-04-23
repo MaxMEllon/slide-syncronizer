55cc326ca581af09d4c8de62b5c02e4f5a4272a8
MaxMEllon <maxmellon1994@gmail.com>
2018-04-23 21:51:35 +0900

    Modify config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -134,7 +134,25 @@ class ConfigGenerator {
         .add(new webpack.NoEmitOnErrorsPlugin())
     if (this.prod)
       plugins
-        .add(new webpack.optimize.UglifyJsPlugin())
+        .add(new webpack.optimize.UglifyJsPlugin({
+          sourceMap: false,
+          compress: {
+            sequences: true,
+            dead_code: true,
+            conditionals: true,
+            booleans: true,
+            unused: true,
+            if_return: true,
+            join_vars: true,
+            drop_console: true
+          },
+          mangle: {
+            except: ['$super', '$', 'exports', 'require']
+          },
+          output: {
+            comments: false
+          }
+        }))
         .add(new CompressionPlugin())
     return plugins
     /* prettier-enable */
